af5ef6f569b25b481e5992bee9b0ccd3e7b00f47
kibbi-gamedev <kibbitech@gmail.com>
2025-03-04 06:35:13 +0700

    Added base href for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import WebPortal from "./pages/WebPortal";
+import WebPortal from "./pages/WebPortal"; // Replace with your actual WebPortal component
 
 function App() {
   // Check if the user is logged in
   const isLoggedIn = localStorage.getItem("isLoggedIn");
 
   return (
-    <Router basename="/webportal">
+    <Router basename="/webportal">  {/* Set basename for GitHub Pages */}
       <Routes>
         {/* Default route: if logged in, go to /webportal, else go to /login */}
         <Route
